929f0076c660a63a3bd75e7255ee5c5b37053552
Jameson Haas <jdhaas59@gmail.com>
2024-03-08 23:07:44 -0500

    make db entries of readings

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -10,6 +10,8 @@ from deck import draw
 from database import create_supabase_client
 from models import User, Test
 
+# from gotrue.exceptions import APIError
+
 
 
 app = FastAPI()
@@ -22,8 +24,20 @@ def root():
 
  
 @app.get('/draw')
-def draw_card():
-    return draw()
+async def draw_card():
+    reading = draw()
+
+    data = sb.table("readings").insert({
+        "user_id":1,
+        "first_card_name": reading[0].Card,
+        "second_card_name": reading[1].Card,
+        "third_card_name": reading[2].Card,
+        "first_card_desc": reading[0].Meaning,
+        "second_card_desc": reading[1].Meaning,
+        "third_card_desc": reading[2].Meaning,
+        }).execute()
+
+    return data
 
 
 @app.post("/test-add")
@@ -45,7 +59,21 @@ async def create_user(user_data: User):
 @app.post("/sign-in")
 async def sign_in(user_data: User):
 
+    # try:
+    #     session = sb.auth.sign_in_with_password({ "email": user_data.email, "password": user_data.password })
+    # except APIError:
+    #     print('Login Failed')
     session = sb.auth.sign_in_with_password({ "email": user_data.email, "password": user_data.password })
-
     return session
+
+
+@app.post("/sign-out")
+async def sign_out(user_data: User):
+
+    # try:
+    #     session = sb.auth.auth.sign_out({ "email": user_data.email, "password": user_data.password })
+    # except APIError:
+    #     print('Logout Failed')
+    res = sb.auth.sign_out({ "email": user_data.email, "password": user_data.password })
+    return res
 
